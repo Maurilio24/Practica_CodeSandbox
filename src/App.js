b29bc11a16c0351b42e675f6927338b9ad64656b
Maurilio24 <119615474+Maurilio24@users.noreply.github.com>
2023-01-13 15:13:44 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Mi práctica en CodeSandbox</h1>
+      <h2>Mauro Omar Zaldivar Flores</h2>
+      <h2>Universidad Tecnologica de Durango</h2>
+    </div>
+  );
+}
